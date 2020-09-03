d087e268675cb24e6e13be390b1070aeb419569c
AgusDOLARD <agust_22_10@hotmail.com>
2020-09-03 05:03:42 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export ZSH="$HOME/.config/oh-my-zsh"
+export ZSH_CUSTOM="$ZSH/custom"
+
+sudo apt install zsh git stow
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+mkdir -pv $HOME/.cache/zsh
+touch $HOME/.cache/zsh/history $HOME/.cache/z
+rm $HOME/.zsh*
+
+git clone https://github.com/AgusDOLARD/dotfiles.git $HOME/.dots
+cd $HOME/.dots
+stow -vt ~ zsh
