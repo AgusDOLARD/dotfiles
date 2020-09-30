e96c009af48bbc22dc1134babd9be0b92a47a7e8
AgusDOLARD <agust_22_10@hotmail.com>
2020-09-30 03:03:42 -0300

    marks

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-export ZSH="$HOME/.config/oh-my-zsh"
-export ZSH_CUSTOM="$ZSH/custom"
-
 PACKAGES='zsh git stow neovim curl'
 
 sudo pacman -S $PACKAGES
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 mkdir -pv $HOME/.cache/zsh
 touch $HOME/.cache/zsh/history $HOME/.cache/z
