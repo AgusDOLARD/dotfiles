92726b7041447c769eeceeaf8eefcc2426f247ba
AgusDOLARD <agust_22_10@hotmail.com>
2020-10-27 06:18:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ mkdir -pv $HOME/.cache/zsh
 touch $HOME/.cache/zsh/history $HOME/.cache/z
 rm $HOME/.zsh*
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 git clone https://github.com/AgusDOLARD/dotfiles.git $HOME/.dots
 cd $HOME/.dots
 stow -vt ~ zsh nvim
